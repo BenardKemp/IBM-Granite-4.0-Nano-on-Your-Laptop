b024a6d9b243933ca7454113b85eac4ba9aa7ee0
FigmaPrompt <ben.kemp.alida@gmail.com>
2025-11-22 14:04:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
+import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_name = "ibm-granite/granite-4.0-micro"
-
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    device_map="cpu",
-)
-
-inputs = tokenizer("Explain how edge AI works:", return_tensors="pt")
-outputs = model.generate(**inputs, max_new_tokens=150)
-print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+device = "cuda"
+model_path = "ibm-granite/granite-4.0-h-350M"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+# drop device_map if running on CPU
+model = AutoModelForCausalLM.from_pretrained(model_path)
+model.eval()
+# change input text as desired
+chat = [
+    { "role": "user", "content": "Please list one IBM Research laboratory located in the United States. You should only output its name and location." },
+]
+chat = tokenizer.apply_chat_template(chat, tokenize=False, add_generation_prompt=True)
+# tokenize the text
+input_tokens = tokenizer(chat, return_tensors="pt")
+# generate output tokens
+output = model.generate(**input_tokens, 
+                        max_new_tokens=100)
+# decode output tokens into text
+output = tokenizer.batch_decode(output)
+# print output
+print(output[0])
